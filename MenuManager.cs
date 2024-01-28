8c6988f09fa95a7280e65f747c9619967b892a10
Max Romagnoli <info@maxromagnoli.com>
2024-01-28 09:55:04 +0100

    Added quit game function to MenuManager.cs

--- MenuManager.cs
+++ MenuManager.cs
@@ -1,10 +1,20 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEditor;
 
 public class MenuManager : MonoBehaviour
 {
     public void PlayLevel(string level_name)
     {
         SceneManager.LoadScene(level_name);
+    }
+
+    public void QuitGame()
+    {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
     }
 }
